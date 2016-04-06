20be48a196d1e8190296b93cdf40229f0fa50b38
abudaan <daniel@abumarkub.net>
2016-04-06 17:05:10 +0200

    update

--- server.es6
+++ server.es6
@@ -1,7 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import request from 'request';
 
 let app = express()
+let gcmUrl = 'https://gcm-http.googleapis.com/gcm/send';
+let gcmKey = 'AIzaSyD6GYalxuGLWy-oMvw3HixS_9ecs_RNFNI'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -11,18 +14,34 @@ app.use(bodyParser.urlencoded({
 
 app.post("/commit", function(req, res) {
 
-  let data
+  let data = req.body
+  let msg = `new commit from ${data.sender.login} to repository ${data.repository.full_name} at ${data.repository.pushed_at}`
 
-  if(req.body.object_kind) {
-    // gitlab payload JSON
-    data = req.body
-  }else{
-    // github payload JSON
-    //data = JSON.parse(req.body.payload)
+  res.send({status: 'SUCCESS'})
+
+  let options = {
+    uri: gcmUrl,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'key=AIzaSyD6GYalxuGLWy-oMvw3HixS_9ecs_RNFNI'
+    },
+    method: 'POST',
+    json: {
+      //'registration_ids': ['73986316761'],
+      'to' : '/topics/global',
+      'data': {
+        'message': msg
+      }
+    }
   }
-  console.log(req.body)
 
-  res.send({ status: 'SUCCESS' })
+  request(options, function(err, res, body){
+    if(err){
+      console.log('err:', err)
+    }
+    //console.log('res:', res)
+    console.log('body:', body)
+  });
 })
 
 let port = process.env.PORT || 5000;
--- server.js
+++ server.js
@@ -10,7 +10,13 @@ var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireWildcard(_bodyParser);
 
+var _request = require('request');
+
+var _request2 = _interopRequireWildcard(_request);
+
 var app = _express2['default']();
+var gcmUrl = 'https://gcm-http.googleapis.com/gcm/send';
+var gcmKey = 'AIzaSyD6GYalxuGLWy-oMvw3HixS_9ecs_RNFNI';
 
 app.use(_bodyParser2['default'].json());
 app.use(_bodyParser2['default'].urlencoded({
@@ -19,21 +25,37 @@ app.use(_bodyParser2['default'].urlencoded({
 
 app.post('/commit', function (req, res) {
 
-  var data = undefined;
-
-  if (req.body.object_kind) {
-    // gitlab payload JSON
-    data = req.body;
-  } else {}
-  console.log(req.body);
+  var data = req.body;
+  var msg = 'new commit from ' + data.sender.login + ' to repository ' + data.repository.full_name + ' at ' + data.repository.pushed_at;
 
   res.send({ status: 'SUCCESS' });
+
+  var options = {
+    uri: gcmUrl,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'key=AIzaSyD6GYalxuGLWy-oMvw3HixS_9ecs_RNFNI'
+    },
+    method: 'POST',
+    json: {
+      //'registration_ids': ['73986316761'],
+      to: '/topics/global',
+      data: {
+        message: msg
+      }
+    }
+  };
+
+  _request2['default'](options, function (err, res, body) {
+    if (err) {
+      console.log('err:', err);
+    }
+    //console.log('res:', res)
+    console.log('body:', body);
+  });
 });
 
 var port = process.env.PORT || 5000;
 
 app.listen(port);
 console.log('server listening at port ' + port);
-
-// github payload JSON
-//data = JSON.parse(req.body.payload)
