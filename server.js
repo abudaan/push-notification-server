297e6426505a0a09e2dde16601435fbdd8c415de
abudaan <daniel@abumarkub.net>
2016-04-06 15:03:31 +0200

    es5

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+//import express from 'express'
+//import bodyParser from 'body-parser'
 
-let app = express()
+//let app = express()
+
+var express = require('express')
+var bodyParser = require('body-parser')
+
+//let app = express()
+var app = express()
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
